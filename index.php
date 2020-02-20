dcb01343a151997ca687365bb1f86d782afb14fa
liuxiaoyu-git <xiaoyliu@redhat.com>
2020-02-20 15:45:10 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,35 @@
 <html>
-	<body>
-	<style>
-			.tupian{ border:0px; width:200px; height:200px}
-		.tupian img{width:200px; height:200px} 
-	</style>
-	<div>
-		<table width=100% height=100%>
-			<tr><td colspan=3 align=center valign=center><font size=60>请为iPhone选择配件</font></td></tr>
-			<tr>
-				<td align=center>
-					<image src=https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MWYK2?wid=445&hei=445&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1567304928359 />
-				</td>
+    <body>
+    <style>
+        .tupian{ border:0px; width:100%; height:1000px}
+        .tupian img{width:300px; height:300px} 
+    </style>
+    <div class=tupian>
+        <table width=60% height=80% align=center>
+            <tr><td colspan=3 align=center valign=center><font size=60><br/>请为iPhone选择配件<p/></font></td></tr>
+            <tr>
                 <td align=center>
-					<image src=https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/HMQB2?wid=445&hei=445&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1550702201599 />
+                    <image src=https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MWYK2?wid=445&hei=445&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1567304928359 />
                 </td>
                 <td align=center>
-					<image src=https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MWVN2?wid=445&hei=445&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1572374637668 />
-				</td>
-			</tr>
-			<tr>
-				<td align=center>
-					<image src=https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MWYA2?wid=445&hei=445&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1566853250388 />
+                    <image src=https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/HMQB2?wid=445&hei=445&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1550702201599 />
                 </td>
                 <td align=center>
-					<image src=https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/HNUP2?wid=445&hei=445&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1579028210281 />
-				</td>
-                <td align=center>
-					<image src=https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/HNLG2?wid=445&hei=445&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1573860981495 />
-				</td>
+                    <image src=https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MWVN2?wid=445&hei=445&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1572374637668 />
+                </td>
             </tr>
-		</table>
-	</div>
-	</body>
+            <tr>
+                <td align=center>
+                    <image src=https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MWYA2?wid=445&hei=445&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1566853250388 />
+                </td>
+                <td align=center>
+                    <image src=https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/HNUP2?wid=445&hei=445&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1579028210281 />
+                </td>
+                <td align=center>
+                    <image src=https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/HNLG2?wid=445&hei=445&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1573860981495 />
+                </td>
+            </tr>
+        </table>
+    </div>
+    </body>
 </html>
